210274a51dea829b517295b64dc9824f9c6a1e55
Kemeice Parson <kem@kemstech.com>
2025-03-30 12:24:01 -0400

    Update answer.py
    answer_thinking

--- portal/answer.py
+++ portal/answer.py
@@ -7,4 +7,29 @@ from langchain_core.messages import HumanMessage, SystemMessage
 
 
 def answer_thinking(question, options, user_response, answer, region):
-    return ""
+    def answer_thinking(question, options, user_response, answer, region):
+    try:
+        llm = VertexAI(model_name="gemini-2.0-flash-thinking-exp-01-21",location=region)
+        
+        input_msg = HumanMessage(content=[f"Here the question{question}, here are the available options {options}, this student's answer {user_response}, whereas the correct answer is {answer}"])
+        prompt_template = ChatPromptTemplate.from_messages(
+            [
+                SystemMessage(
+                    content=(
+                        "You are a helpful teacher trying to teach the student on question, you were given the question and a set of multiple choices "
+                        "what's the correct answer. use friendly tone"
+                    )
+                ),
+                input_msg,
+            ]
+        )
+
+        prompt = prompt_template.format()
+        
+        response = llm.invoke(prompt)
+        print(f"response: {response}")
+
+        return response
+    except Exception as e:
+        print(f"Error sending message to chatbot: {e}") # Log this error too!
+        return f"Unable to process your request at this time. Due to the following reason: {str(e)}"
