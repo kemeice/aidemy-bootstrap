80b66e58b825002eaed7d664f03dfd356c6d4fe1
Kemeice Parson <kem@kemstech.com>
2025-03-30 11:46:50 -0400

    Update app.py
    create plan

--- planner/app.py
+++ planner/app.py
@@ -7,7 +7,24 @@ from google.cloud import pubsub_v1
 app = Flask(__name__)
 PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
 
-##ADD SEND PLAN EVENT FUNCTION HERE
+def send_plan_event(teaching_plan:str):
+    """
+    Send the teaching event to the topic called plan
+    
+    Args:
+        teaching_plan: teaching plan
+    """
+    publisher = pubsub_v1.PublisherClient()
+    print(f"-------------> Sending event to topic plan: {teaching_plan}")
+    topic_path = publisher.topic_path(PROJECT_ID, "plan")
+
+    message_data = {"teaching_plan": teaching_plan} 
+    data = json.dumps(message_data).encode("utf-8") 
+
+    future = publisher.publish(topic_path, data)
+
+    return f"Published message ID: {future.result()}"
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -30,7 +47,7 @@ def index():
             """
         )
 
-        ### ADD send_plan_event CALL
+        send_plan_event(teaching_plan)
         
         return jsonify({'teaching_plan': teaching_plan})
     return render_template('index.html', years=years, subjects=subjects, teaching_plan=None, assignment=None)
