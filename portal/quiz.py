9139256df64cb1f2d8c770779c2ce54fb309a407
Kemeice Parson <kem@kemstech.com>
2025-03-30 12:12:04 -0400

    Update quiz.py
    generate_quiz

--- portal/quiz.py
+++ portal/quiz.py
@@ -13,6 +13,44 @@ class QuizQuestion(BaseModel):
 
 # ENV SETUP
 project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")  # Get project ID from env
+def generate_quiz_question(file_name: str, difficulty: str, region:str ):
+    """Generates a single multiple-choice quiz question using the LLM.
+   
+    ```json
+    {
+      "question": "The question itself",
+      "options": ["Option A", "Option B", "Option C", "Option D"],
+      "answer": "The correct answer letter (A, B, C, or D)"
+    }
+    ```
+    """
+
+    print(f"region: {region}")
+    # Connect to resourse needed from Google Cloud
+    llm = VertexAI(model_name="gemini-1.5-pro", location=region)
+
+
+    plan=None
+    #load the file using file_name and read content into string call plan
+    with open(file_name, 'r') as f:
+        plan = f.read()
+
+    parser = JsonOutputParser(pydantic_object=QuizQuestion)
+
+
+    instruction = f"You'll provide one question with difficulty level of {difficulty}, 4 options as multiple choices and provide the anwsers, the quiz needs to be related to the teaching plan {plan}"
+
+    prompt = PromptTemplate(
+        template="Generates a single multiple-choice quiz question\n {format_instructions}\n  {instruction}\n",
+        input_variables=["instruction"],
+        partial_variables={"format_instructions": parser.get_format_instructions()},
+    )
+    
+    chain = prompt | llm | parser
+    response = chain.invoke({"instruction": instruction})
+
+    #print(f"{response}")
+    return  response
 
 
 
